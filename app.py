f000b1c8685f7e3ddc00fa5a023b7960e8681268
magalhaestiago <magalhaes_tiago@outlook.com>
2023-12-08 09:05:54 -0300

    funcoes de selecao

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from files import *
-from funcoes import *
+from funcoes2 import *
 
 
 
@@ -9,8 +9,23 @@ data = readFile(arquivo)
 
 # Gerar lista de imc com os arquivos lidos
 lista = gerar_lista_imc(data)
+lista_acima_media = selecao_acima_media(data)
+lista_abaixo_media = selecao_abaixo_media(data)
 
-# Criar arquivo .json
+print("Lista completa: ", lista)
+print("Lista de IMC's acima da média: ", lista_acima_media)
+print("Lista de IMC's abaixo da média: ", lista_abaixo_media)
+
+# Criar arquivos .json
 nome = 'lista_imc.json'
-createFile(lista,nome)
+createFile(lista, nome)
 
+# Acima da média
+nome = 'lista_imc_acima_media.json'
+createFile(lista_acima_media,nome)
+
+# Abaixo da média
+nome = 'lista_imc_abaixo_media.json'
+createFile(lista_abaixo_media, nome)
+
+nome = ''
--- funcoes.py
+++ funcoes.py
@@ -1,15 +1,117 @@
 from files import *
 
+# Método para calculo imc (fórmula retirada do google)
 def calculo_imc(peso, altura):
     return peso / (altura * altura)
 
+# Método para gerar lista IMC derivado de um dicionário JSON seguindo o padrão do arquivo 'pessoas.json' dentro desse diretório
 def gerar_lista_imc(dados):
-    tamanho_lista = len(dados['pessoas'])
-    lista_imc_pessoas = {}
-    for i in range(tamanho_lista):
-        aux = calculo_imc(dados['pessoas'][i]['peso'],dados['pessoas'][i]['altura'])
-        lista_imc_pessoas[dados['pessoas'][i]['nome']] = aux
+    tamanho_lista = len(dados['pessoas']) # Variável para armazenar a quantidade de 'pessoas' ou 'clientes'
+    lista_imc_pessoas = {} 
+    for i in range(tamanho_lista): 
+        aux = calculo_imc(dados['pessoas'][i]['peso'],dados['pessoas'][i]['altura']) # Calcular o imc do iteravel
+        lista_imc_pessoas[dados['pessoas'][i]['id']] = [dados['pessoas'][i]['nome'], aux] # Armazenar o imc calculado acima
 
+    # Retorna a lista com IMC's calculados
     return lista_imc_pessoas
 
 
+
+
+def selecao_acima_media(dados):
+    aux = gerar_lista_imc(dados)
+    
+    avg = 0
+    sum = 0
+    tam_dados = len(aux)
+    
+    # Calcula a soma de IMC's
+    for i in range(1, tam_dados + 1):
+        sum = sum + aux[i][1]
+    
+    # Calcula a média de IMC's
+    avg = sum / tam_dados
+    
+    # Se IMC[i] estiver abaixo da média, é removido da lista
+    for i in range(1, tam_dados + 1):
+        if aux[i][1] < avg:
+            del aux[i]
+            
+    # Retorna nova lista com os IMC's acima da média
+    return aux
+
+
+
+def selecao_abaixo_media(dados):
+    aux = gerar_lista_imc(dados)
+    
+    aux = gerar_lista_imc(dados)
+    
+    avg = 0
+    sum = 0
+    tam_dados = len(aux)
+    
+    # Calcula a soma de IMC's
+    for i in range(1, tam_dados + 1):
+        sum = sum + aux[i][1]
+    
+    # Calcula a média de IMC's
+    avg = sum / tam_dados
+    
+    # Se IMC[i] estiver acima da média é removido
+    for i in range(1, tam_dados + 1):
+        if aux[i][1] > avg:
+            del aux[i]
+            
+    # Retorna lista com IMC's abaixo da média
+    return aux
+            
+            
+            
+    
+# !!!! As funções abaixo ainda estão sendo desenvolvidas, para que possam retornar além da lista de IMC's, também informações importantes, como a média que foi estabelecida, quantidade de elementos que foram removidos, e dentre outros !!!!
+
+def selecao_abaixo_media_informacoes(dados):
+    aux = gerar_lista_imc(dados)
+    
+    aux = gerar_lista_imc(dados)
+    
+    avg = 0
+    sum = 0
+    tam_dados = len(aux)
+    elem_removido = 0
+    
+    for i in range(1, tam_dados + 1):
+        sum = sum + aux[i][1]
+        
+    avg = sum / tam_dados
+    
+    for i in range(1, tam_dados + 1):
+        if aux[i][1] > avg:
+            elem_removido = i + 1
+            del aux[i]
+            
+    
+    return elem_removido, avg, aux
+
+def selecao_acima_media_informacoes(dados):
+    aux = gerar_lista_imc(dados)
+    
+    avg = 0
+    sum = 0
+    tam_dados = len(aux)
+    elem_removido = 0
+    
+    
+    for i in range(1, tam_dados + 1):
+        sum = sum + aux[i][1]
+        
+    avg = sum / tam_dados
+    
+    for i in range(1, tam_dados + 1):
+        if aux[i][1] < avg:
+            elem_removido = i
+            del aux[i]
+            
+    
+    return elem_removido, avg, aux
